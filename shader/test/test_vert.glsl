c36c4afb80e2199dbd7c9af9f0628a797642307d
Kreisel Sebastian <kreisel.sebastian@gmail.com>
2013-02-27 18:12:01 +0100

    Major reorganisation and renaming some packages and classes!

---
+++ shader/test/test_vert.glsl
@@ -0,0 +1,18 @@
+#version 140
+
+in vec4 pos_model;
+in vec4 pos_color;
+
+uniform mat4 mvp_matrix;
+uniform vec2 offset;
+
+out vec4 color;
+
+
+void main() {
+	vec4 position = pos_model * mvp_matrix;
+	position[0] += offset[0]
+	position[1] += offset[1];
+	gl_Position = position;
+	color = pos_color;
+}
